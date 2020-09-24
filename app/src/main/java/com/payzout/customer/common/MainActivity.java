e1d4b8311e81ef0e108145c6c695f5c948cdd09b
Sahil Singh <sahilsingh.dsc@gmail.com>
2020-09-24 16:25:24 +0530

    firebase auth issue fixed

---
+++ app/src/main/java/com/payzout/customer/common/MainActivity.java
@@ -0,0 +1,26 @@
+package com.payzout.customer.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.payzout.customer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        initView();
+    }
+
+    private void initView() {
+    }
+
+}
