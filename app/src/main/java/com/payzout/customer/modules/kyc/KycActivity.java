a6149927ff04daa83106e161d326f7f1a0772cf8
Sahil Singh <sahilsingh.dsc@gmail.com>
2020-09-30 12:08:28 +0530

    kyc screen implemented 123

---
+++ app/src/main/java/com/payzout/customer/modules/kyc/KycActivity.java
@@ -0,0 +1,36 @@
+package com.payzout.customer.modules.kyc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.payzout.customer.R;
+import com.payzout.customer.modules.kyc.basic.BasicDetailsActivity;
+
+public class KycActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout lvBasicDetails;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kyc);
+        initView();
+    }
+
+    private void initView() {
+        lvBasicDetails = findViewById(R.id.lvBasicDetails);
+        lvBasicDetails.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == lvBasicDetails) {
+            Intent intent = new Intent(KycActivity.this, BasicDetailsActivity.class);
+            startActivity(intent);
+        }
+    }
+}
